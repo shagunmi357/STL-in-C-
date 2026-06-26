5c4da787230b6b2cb019b39ccca1cc0ff7ab0d6e
Shagun <shagunmi357@gmail.com>
2026-06-26 19:05:25 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v(2,9);
+    for ( int x : v){
+        cout << x  << endl;
+
+    vector<int> v2 = v;
+    for (int s : v){
+        cout << s << endl;
+    }
+   
+
+    }
+
+    return 0;
+}
